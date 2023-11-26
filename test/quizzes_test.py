7ef754df67ef1e9fb1cc1a1c1ec6102599910e28
Harsh Vanasiwala <81178088+HarshVanasiwala-18@users.noreply.github.com>
2023-11-26 10:28:46 -0500

    Updated test_expose_failure function
    Changed function test_expose_failure_02 and test_expose_failure_03

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,20 +1,30 @@
 import unittest
-
 from app.controllers.quizzes_controller import QuizzesController
+from datetime import datetime
 
 class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
-        
+    
     def test_expose_failure_01(self):
-        """
-        Implement this function and two more that
-        execute the code and make it fail.
-        """
-        self.assertTrue(True, 'Example assertion.')
-        
+        self.ctrl.clear_data()
+
+        self.ctrl.add_quiz("Quiz Title", "quiz text", "invalid start date", datetime(2023, 3, 18, 12, 30, 0))
+        quizzes = self.ctrl.get_quizzes()
+
+        self.assertEqual(len(quizzes), 0, "There should be no quizzes as it should have thrown an exception saying invalid date.")
+
+    def test_expose_failure_02(self):
+        self.ctrl.add_quiz(None, None, None, None)
+
+    def test_expose_failure_03(self):
+        self.ctrl.clear_data()
+        quiz_id = self.ctrl.add_quiz("Quiz Title", "quiz text", datetime(2023, 2, 15, 12, 30, 0), datetime(2023, 12, 15, 12, 30, 0))
+        question_id = self.ctrl.add_question(quiz_id, "title", "question text")
+        answer_id = self.ctrl.add_answer(question_id, "this is the answer", "Wrong type of Variable exposing the failure Should have checked whether it is boolean or not")
+        self.assertNone(answer_id, "Answer Id should not have been generated ")
 
 if __name__ == '__main__':
     unittest.main()
